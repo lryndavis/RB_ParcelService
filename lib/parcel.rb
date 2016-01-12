bec6fbea6b744b5a4476220b76da358e0054ea1c
Lauryn Davis and Mike Mahoney <lryndavis@gmail.com>
2016-01-11 16:48:27 -0800

    Create rate over distance base rate

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,13 +1,41 @@
 class Parcel
+  require('pry')
 
-  define_method(:initialize) do |length, width, height, weight|
+  define_method(:initialize) do |length, width, height, weight, distance|
     @length = length
     @width = width
     @height = height
     @weight = weight
+    @distance = distance
   end
 
   define_method(:volume) do
+    return nil unless valid_entry?
     (@length * @width * @height).round(1)
+  end
+
+  define_method(:valid_entry?) do
+    (((@length.is_a?(Float) || @length.is_a?(Integer) && @length > 0)) &&\
+     ((@width.is_a?(Float) || @width.is_a?(Integer) && @width > 0)) &&\
+      ((@height.is_a?(Float) || @height.is_a?(Integer)) && @height > 0))
+  end
+
+  define_method(:cost_to_ship) do
+    base_rate = { :br_distance => 100, :br_volume => 10 }
+    base_rate_plus = { :br => 20, :per_mile => 25, :per_volume => 10 }
+
+    if base_rate?(base_rate)
+      return base_rate_plus.fetch(:br)
+    else
+      return (distance(base_rate) * (base_rate_plus[:per_mile] * 0.01) + base_rate_plus[:br])
+    end
+  end
+
+  define_method(:base_rate?) do |base_rate|
+      @distance < base_rate[:br_distance] && volume < base_rate[:br_volume]
+  end
+
+  define_method(:distance) do |base_rate|
+    @distance - base_rate[:br_distance]
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,13 +4,32 @@ require('parcel')
 describe(Parcel) do
   describe('volume') do
     it('will return the volume based on the product of the sides') do
-      test_volume = Parcel.new(2,2,6,10)
+      test_volume = Parcel.new(2,2,6,10, 10)
       expect(test_volume.volume()).to(eq(24))
     end
 
     it('will return the volume if floats are input') do
-      test_volume = Parcel.new(2.2, 2.3, 5, 5.5)
+      test_volume = Parcel.new(2.2, 2.3, 5, 5.5, 10)
       expect(test_volume.volume()).to(eq(25.3))
     end
+    it('will return "nil" if a zero is in any input') do
+      test_volume = Parcel.new(0,2,2,2,10)
+      expect(test_volume.volume()).to(eq(nil))
+    end
+    it('will return "nil" if a non number is an input') do
+      test_volume = Parcel.new("p",2,2,2,10)
+      expect(test_volume.volume()).to(eq(nil))
+    end
+  end
+
+  describe('rates') do
+    it('will return base rate if applicable') do
+      test_rate = Parcel.new(2,2,2,2,50)
+      expect(test_rate.cost_to_ship()).to(eq(20))
+    end
+  end
+  it('will calculate a new rate if distance is greater than 100') do
+    test_rate = Parcel.new(2,2,2,2,150)
+    expect(test_rate.cost_to_ship()).to(eq(32.50))
   end
 end
